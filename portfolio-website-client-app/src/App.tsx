867d08b6d829351c76272d4ea11f43030fc8d77d
Priyanshu Yashwant Deshmukh <69320370+priyansh4320@users.noreply.github.com>
2025-02-08 23:06:14 +0530

    feat: added a 3d canvas to app.

--- portfolio-website-client-app/src/App.tsx
+++ portfolio-website-client-app/src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Canvas } from '@react-three/fiber';
+import { Text } from '@react-three/drei';
 import './App.css'
 
-function App() {
-  
 
+
+function App() {
   return (
-    <>
-      <div>
-       <p>Hello world</p> 
-        
-      </div>
-      
-    </>
+   <>
+   <Canvas camera={{ position: [0, 0, 5] }} 
+   style={{
+    border:"solid 2px black",
+    width:"100%",
+    height:"100vh",
+    background:"rgba(0,0,0,0.1)"
+   }}
+   >
+      <Text
+      fontSize={.5} 
+      color="black"
+      position={[-3.5,3.25,0]}
+      >
+        Hello, I'm An AI Engineer
+      </Text>
+  </Canvas>
+  </>
   )
 }
-
 export default App
