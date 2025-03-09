f0f60c10d94e2b6a31efe26b20cb74356319a860
Priyanshu Yashwant Deshmukh <69320370+priyansh4320@users.noreply.github.com>
2025-03-09 23:31:53 +0530

    feat: Migrate to MUI
    feat: multiple bananas.
    
    clean up projecrt

--- portfolio-website-client-app/src/App.tsx
+++ portfolio-website-client-app/src/App.tsx
@@ -1,50 +1,30 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls ,useGLTF} from '@react-three/drei';
-import { useRef,useState } from 'react';
+
 import './App.css'
-
-const Banana = ({position}:{position:[number,number,number]}) =>{
-  
-  const {scene} = useGLTF("/banana.glb");
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const bananaRef = useRef<any>(0);
-  const [y , setY] = useState(position[1]);
-  const [rotation, setRotation] = useState([0, 0, 0]); 
-
-  useFrame(() => {
-    if (bananaRef.current) {
-      if (y>-2) {
-        setY((prevY)=>prevY-0.05);
-        }
-      else{
-          setY(5);
-        }
-        bananaRef.current.position.y = y;
-
-        setRotation(([rx, ry, rz]) => [rx + 0.02, ry + 0.03, rz + 0.01]);
-        bananaRef.current.rotation.set(...rotation);
-      
-    }
-  });
-  return <primitive ref={bananaRef} object={scene} position={[position[0], y, position[2]]} scale={10} />;
-}
-
+import { AppBar } from '@mui/material';
+import {Toolbar} from '@mui/material';
+import {Typography} from '@mui/material';
+import {List,ListItem , ListItemText} from '@mui/material';
+import {Box} from '@mui/material';
 const App = () => {
+  
   return (
-   <>
-   <Canvas camera={{ position: [0, 0, 5] }} 
-   style={{
-    border:"solid 2px black",
-    width:"100%",
-    height:"100vh",
-    background:"rgba(0,0,0,.1)",
-   }}
-   >
-    <ambientLight intensity={.5} />
-    <pointLight position={[1,1,1]}/>
-      <Banana  position={[0, 5, 0]}/>
-      <OrbitControls/>
-  </Canvas>  
+  <>
+  <AppBar sx={{background:"#1A1A1A"}}>
+    <Toolbar sx={{display:'flex',justifyContent:'space-between'}}>
+      <Typography  variant='h1' sx={{fontFamily:"montez, cursive",fontSize:"30px"}}> Priyanshu</Typography>
+      <Box component="nav" >
+
+        <List sx ={{display:"flex",justifyContent:"space-between"}}>
+        {["Home", "Resume", "Portfolio", "Blog", "Content"].map((item)=>(
+          <ListItem key={item}>
+              <ListItemText primary={item} sx={{fontFamily:"'Roboto Mono', monospace",color:"white",px:"10px",py:"2px", fontSize:"10px"}}>
+              </ListItemText>
+          </ListItem>
+        ))}
+        </List> 
+      </Box>
+    </Toolbar>
+  </AppBar>
   </>
   )
 }
