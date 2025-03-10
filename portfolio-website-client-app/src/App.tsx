68b69dde73ed505599157dab19a9ad1dc461aa22
Priyanshu Yashwant Deshmukh <69320370+priyansh4320@users.noreply.github.com>
2025-03-11 01:20:07 +0530

    feat: add background image.

--- portfolio-website-client-app/src/App.tsx
+++ portfolio-website-client-app/src/App.tsx
@@ -9,22 +9,32 @@ const App = () => {
   
   return (
   <>
-  <AppBar sx={{background:"#1A1A1A"}}>
-    <Toolbar sx={{display:'flex',justifyContent:'space-between'}}>
-      <Typography  variant='h1' sx={{fontFamily:"montez, cursive",fontSize:"30px"}}> Priyanshu</Typography>
-      <Box component="nav" >
-
-        <List sx ={{display:"flex",justifyContent:"space-between"}}>
-        {["Home", "Resume", "Portfolio", "Blog", "Content"].map((item)=>(
-          <ListItem key={item}>
-              <ListItemText primary={item} sx={{fontFamily:"'Roboto Mono', monospace",color:"white",px:"10px",py:"2px", fontSize:"10px"}}>
-              </ListItemText>
-          </ListItem>
-        ))}
-        </List> 
-      </Box>
-    </Toolbar>
-  </AppBar>
+  <Box sx={{
+    width:"100%",
+    height:"100vh",
+    alignItems:"center", 
+    backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.9)), url('priyanshu3.png')", 
+    backgroundColor:"rgba(0,0,0,.5)",
+    backgroundSize:"contain",
+    backgroundRepeat:"no-repeat",
+    backgroundPosition:"right",
+    }}>
+    <AppBar sx={{backgroundColor:"rgba(0,0,0,0.0)"}}>
+      <Toolbar sx={{display:'flex',justifyContent:'space-between'}}>
+        <Typography  variant='h1' sx={{fontFamily:"montez, cursive",fontSize:"30px"}}> Priyanshu</Typography>
+        <Box component="nav" >
+          <List sx ={{display:"flex",justifyContent:"space-between"}}>
+          {["Home", "Resume", "Portfolio", "Blog", "Content"].map((item)=>(
+            <ListItem key={item}>
+                <ListItemText primary={item} sx={{fontFamily:"'Roboto Mono', monospace",color:"white",px:"10px",py:"2px", fontSize:"10px"}}>
+                </ListItemText>
+            </ListItem>
+          ))}
+          </List> 
+        </Box>
+      </Toolbar>
+    </AppBar>
+  </Box>
   </>
   )
 }
